49025d441631af5d0db6d5c7be5234595fd92418
nazem0_home_pc <mohamednazem09@gmail.com>
2024-04-21 20:13:52 +0200

    app helper & validations

---
+++ src/app/helpers/app-helper.ts
@@ -0,0 +1,19 @@
+import { AbstractControl, FormGroup } from '@angular/forms';
+export class AppHelper {
+    public static checkIsFormTouchedOrDirty(form:FormGroup):boolean{
+        return form.touched || form.dirty
+    }
+
+
+    public static checkIsControlTouchedOrDirty(control:AbstractControl<any, any> | null):boolean{
+        return (control?.touched || control?.dirty) ?? false
+    }
+
+    public static checkIsControlHasRequiredError(control:AbstractControl<any, any> | null):boolean{
+        return control?.getError('required')
+    }
+
+    public static checkTouchedOrDirtyAndRequiredError(control:AbstractControl<any, any> | null):boolean{
+        return this.checkIsControlTouchedOrDirty(control) && this.checkIsControlHasRequiredError(control)
+    }
+}
