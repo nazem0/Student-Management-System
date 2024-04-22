30e7c7dd02b3f35a9455a250cf9678fd19c90b44
nazem0_home_pc <mohamednazem09@gmail.com>
2024-04-22 18:13:21 +0200

    edit student empty component

--- src/app/modules/student/student.module.ts
+++ src/app/modules/student/student.module.ts
@@ -4,12 +4,14 @@ import { StudentRoutes } from './student.routing';
 import { StudentsListComponent } from './components/students-list/students-list.component';
 import { StudentService } from './services/student.service';
 import { SharedModule } from '../shared/shared.module';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
 
 
 
 @NgModule({
   declarations: [
-    StudentsListComponent
+    StudentsListComponent,
+    EditStudentComponent
   ],
   imports: [
     CommonModule,
